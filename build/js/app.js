d266069836bad99701f3c4b6c20d278617b181c6
Kelsey Langlois and Megan Olson <kelsey@>
2017-10-09 16:07:21 -0700

    Update to display images.

--- build/js/app.js
+++ build/js/app.js
@@ -55,16 +55,16 @@ $(document).ready(function() {
 
   var col = 0;
   deck.cards.forEach(function(card) {
-    $('.col' + col).append('<div class="card ' + card.picture +'">Card</div>');
+    $('.col' + col).append('<div class="card ' + card.picture +'"><img src="img/' + card.picture + '.jpg"></div>');
     $('.col' + col + ' div').last().click(function() {
       if (!$(this).hasClass('matched')) {
         result = deck.chooseCard(card);
-        $(this).text(card.picture);
+        $(this).find('img').show();
         $(this).addClass('flipped');
         $('.message').text(result);
         if (result == "Not a Match.") {
           setTimeout(function() {
-            $(".flipped").text('Card');
+            $(".flipped img").hide();
             $(".flipped").removeClass('flipped');
           }, 1000);
         } else if (result == "Matched!") {
@@ -81,8 +81,8 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function(){
-  $('#time').text(moment());
-});
+// $(document).ready(function(){
+//   $('#time').text(moment());
+// });
 
 },{"./../js/card.js":1,"./../js/deck.js":2}]},{},[3]);
--- js/card-interface.js
+++ js/card-interface.js
@@ -16,16 +16,16 @@ $(document).ready(function() {
 
   var col = 0;
   deck.cards.forEach(function(card) {
-    $('.col' + col).append('<div class="card ' + card.picture +'">Card</div>');
+    $('.col' + col).append('<div class="card ' + card.picture +'"><img src="img/' + card.picture + '.jpg"></div>');
     $('.col' + col + ' div').last().click(function() {
       if (!$(this).hasClass('matched')) {
         result = deck.chooseCard(card);
-        $(this).text(card.picture);
+        $(this).find('img').show();
         $(this).addClass('flipped');
         $('.message').text(result);
         if (result == "Not a Match.") {
           setTimeout(function() {
-            $(".flipped").text('Card');
+            $(".flipped img").hide();
             $(".flipped").removeClass('flipped');
           }, 1000);
         } else if (result == "Matched!") {
